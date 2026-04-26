c56f50c737ec0f1431ee570a091680f823c59d9d
PhamPhiTruong <21130593@st.hcmuaf.edu.vn>
2026-04-26 17:09:07 +0700

    Fix init

---
+++ BackEnd/dryfood_myapp/src/main/java/nlu/tmdt/dryfood_myapp/DryfoodMyappApplication.java
@@ -0,0 +1,13 @@
+package nlu.tmdt.dryfood_myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DryfoodMyappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DryfoodMyappApplication.class, args);
+    }
+
+}
---
+++ BackEnd/dryfood_myapp/src/test/java/nlu/tmdt/dryfood_myapp/DryfoodMyappApplicationTests.java
@@ -0,0 +1,13 @@
+package nlu.tmdt.dryfood_myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DryfoodMyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
