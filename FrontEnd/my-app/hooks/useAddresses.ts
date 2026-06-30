059536927fea0fbf16afcbceae45f63e51cbd909
NhatKun98 <21130464@st.hcmuaf.edu.vn>
2026-06-30 20:31:43 +0700

    cap nhat giao dien va chuc nang quan ly dia chi nguoi dung (edit useAddress)

--- FrontEnd/my-app/hooks/useAddresses.ts
+++ FrontEnd/my-app/hooks/useAddresses.ts
@@ -39,7 +39,11 @@ export function useAddresses() {
     }
   }, []);
 
+  // useEffect(() => {
+  //   fetchAddresses();
+  // }, [fetchAddresses]);
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     fetchAddresses();
   }, [fetchAddresses]);
 
