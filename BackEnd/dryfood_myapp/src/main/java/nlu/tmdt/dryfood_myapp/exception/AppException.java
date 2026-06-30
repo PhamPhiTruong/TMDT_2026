ba41ff3a5f696099bf3bc8c0c90cf00e70baf067
PhamPhiTruong <21130593@st.hcmuaf.edu.vn>
2026-07-01 00:07:23 +0700

    Fix some local bug

--- BackEnd/dryfood_myapp/src/main/java/nlu/tmdt/dryfood_myapp/exception/AppException.java
+++ BackEnd/dryfood_myapp/src/main/java/nlu/tmdt/dryfood_myapp/exception/AppException.java
@@ -1,13 +1,21 @@
 package nlu.tmdt.dryfood_myapp.exception;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
-@Builder
-@AllArgsConstructor
-@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AppException extends RuntimeException {
     ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public AppException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
 }
